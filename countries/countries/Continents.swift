e88337965c1fc589221d6ae11319109454aab690
Jacob Brauchler <jacob.brauchler@outlook.com>
2017-02-14 19:39:42 -0700

    Project 1 Milestone 1

---
+++ countries/countries/Continents.swift
@@ -0,0 +1,14 @@
+//
+//  Continents.swift
+//  countries
+//
+//  Created by Jacob Brauchler on 2/9/17.
+//  Copyright © 2017 JBrauchler. All rights reserved.
+//
+
+import Foundation
+
+class Continents {
+    var continentData = [String : [String]]()
+    var continents = [String]()
+}
